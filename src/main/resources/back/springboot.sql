82cf2f94be5cbce37f5a8e60b6aed6d38884df42
zhangfan <zhangfan@ycmedia.cn>
2016-11-01 19:26:56 +0800

    sql脚本

---
+++ src/main/resources/back/springboot.sql
@@ -0,0 +1,51 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.6.22-log : Database - springboot
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `student` */
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `ScoreSum` varchar(100) DEFAULT NULL,
+  `ScoreAvg` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `student` */
+
+insert  into `student`(`Id`,`name`,`age`,`ScoreSum`,`ScoreAvg`) values (1,'a',45,'1364','312'),(2,'a',19,'1364','312'),(3,'a',19,'1364','312');
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `Id` int(11) NOT NULL DEFAULT '0',
+  `name` varchar(40) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `qq` varchar(100) DEFAULT NULL,
+  `phone` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+insert  into `user`(`Id`,`name`,`age`,`qq`,`phone`) values (1,'a1',45,'1364','312'),(2,'a2',19,'1364','312'),(3,'a3',19,'1364','312');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
